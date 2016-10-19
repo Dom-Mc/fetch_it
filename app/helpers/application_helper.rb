4c8671a03f810188746889628b2127a1ca186d7c
Dom McKellar <dom.mckellar@gmail.com>
2016-10-19 10:12:05 -0700

    Add helpers to retrieve current year and site title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def current_year
+    Time.now.year
+  end
+
+  # Returns site title or page title + site title
+  def full_title(page_title = "")
+    if page_title.blank?
+      t(:site_name)
+    else
+      "#{page_title} | #{t(:site_name)}"
+    end
+  end
+
 end
