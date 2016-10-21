6692514dd6ce53dfd047b6fc2785b45182e9688a
Dom McKellar <dom.mckellar@gmail.com>
2016-10-20 23:00:57 -0700

    Update strong params for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,34 @@
 class OrdersController < ApplicationController
+  def new
+    # TODO: go over
+    user = User.last
+    @order = user.orders.build
+    @shipper = @order.build_shipper
+    @recipient = @order.build_recipient
+
+  end
+
+  def create
+    binding.pry
+
+    User.last.orders.create(order_params)
+    binding.pry
+    raise order_params.inspect
+  end
+
+  private
+
+    def order_params
+      params.require(:order).permit(
+        :number_of_items,
+        :total_charge,
+        :special_instructions,
+        :shipping_reference,
+        :estimated_weight,
+        :service_id,
+        shipper_attributes: [:first_name, :last_name, address: [:address_type, :street_address, :second_address, :zip], phone: [:phone_number, :phone_type, :ext]],
+        recipient_attributes: [:first_name, :last_name, address: [:address_type, :street_address, :second_address, :zip], phone: [:phone_number, :phone_type, :ext]]
+      )
+    end
+
 end
