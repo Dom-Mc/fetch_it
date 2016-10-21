68694a8f1f7ef89656a209e571995b52de466760
Dom McKellar <dom.mckellar@gmail.com>
2016-10-21 09:22:15 -0700

    Create devise helper for flash messages and alter partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,14 @@ module ApplicationHelper
     end
   end
 
+  def flash_class(flash_type)
+    case flash_type
+      when "notice" then "alert alert-dismissable alert-info"
+      when "success" then "alert alert-dismissable alert-success"
+      when "error" then "alert alert-dismissable alert-danger"
+      when "alert" then "alert alert-dismissable alert-danger"
+      else flash_type.to_s
+    end
+  end
+
 end
