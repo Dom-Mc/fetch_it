e36aec6346cf27d9cd187d201456eb106bc555d7
Dom McKellar <dom.mckellar@gmail.com>
2016-10-25 14:39:21 -0700

    Add PST time zone setting

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module FetchIt
       g.stylesheets = false
       g.javascripts = false
     end
+
+    config.time_zone = "Pacific Time (US & Canada)"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
