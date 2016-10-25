87956a82f8059e285ae7cd0190f73dac41a5137b
Dom McKellar <dom.mckellar@gmail.com>
2016-10-25 14:14:59 -0700

    Add helpers to set and display time, date, and service

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,36 @@
 module OrdersHelper
+
+  def past_cuttoff_time?
+    # TODO: change to set endtime variable
+    Date.current.today? && Time.current.hour > 15 ? true : false
+  end
+
+  def set_date
+    if past_cuttoff_time?
+      Date.tomorrow
+    else
+      Date.current
+    end
+  end
+
+  def set_time
+    if Time.current.hour < 9 || Time.current.hour > 15
+      Time.zone.parse("9:00")
+    else
+      1.hour.from_now
+    end
+  end
+
+  def display_date
+    @order.pickup_date ||= set_date
+  end
+
+  def display_time
+    @order.pickup_time ||= set_time
+    Time.zone.parse(@order.pickup_time)
+  end
+
+  def display_service
+    @order.service.service_name || Service.first.service_name
+  end
 end
