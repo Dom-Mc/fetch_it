70a723ca445dc2c33feb7598737f8a7bdb9f441b
Dom McKellar <dom.mckellar@gmail.com>
2016-10-25 14:36:01 -0700

    Fix strong params and refactor new and create actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,43 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-    def index
-    end
+  def index
+    @user = current_user
+  end
 
   def new
-    # TODO: go over
-    user = User.last
-    @order = user.orders.build
+    @order = current_user.orders.build
     @shipper = @order.build_shipper
     @recipient = @order.build_recipient
-
   end
 
   def create
-    binding.pry
+    @order = current_user.orders.build(order_params)
 
-    User.last.orders.create(order_params)
-    binding.pry
-    raise order_params.inspect
+    if @order.save
+      redirect_to @order, notice: 'Your order has been placed.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @order = current_user.orders.find_by(params[:id])
   end
 
   private
-
     def order_params
       params.require(:order).permit(
+        :pickup_date,
+        :get_pickup_time,
         :number_of_items,
         :order_total,
         :special_instructions,
         :shipping_reference,
         :estimated_weight,
         :service_id,
-        shipper_attributes: [:first_name, :last_name, address: [:address_type, :street_address, :second_address, :zip], phone: [:phone_number, :phone_type, :ext]],
-        recipient_attributes: [:first_name, :last_name, address: [:address_type, :street_address, :second_address, :zip], phone: [:phone_number, :phone_type, :ext]]
+        shipper_attributes: [:first_name, :last_name, address: [:address_type, :street_address, :secondary_address, :zip], phone: [:phone_number, :phone_type, :ext]],
+        recipient_attributes: [:first_name, :last_name, address: [:address_type, :street_address, :secondary_address, :zip], phone: [:phone_number, :phone_type, :ext]]
       )
     end
 
