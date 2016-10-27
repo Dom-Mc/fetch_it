8f70ce83f070d5f355137bbe52a5d97970f1a8db
Dom McKellar <dom.mckellar@gmail.com>
2016-10-26 18:10:05 -0700

    Fix time setters and validations

--- app/models/service.rb
+++ app/models/service.rb
@@ -11,6 +11,7 @@
 #  updated_at   :datetime         not null
 
 class Service < ApplicationRecord
+  attr_accessor :get_service_start, :get_service_end
 
   has_many :orders, inverse_of: :service
 
@@ -19,37 +20,37 @@ class Service < ApplicationRecord
 
   validates :end_time, presence: { message: "was blank or contained invalid characters." }
 
-  validates :price, presence: true,
-                    numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0, message: "must be greater than $0.00." }
 
   validates :service_name, presence: true,
                    length: { maximum: 50 },
                    uniqueness: true
 
+
   validates :start_time, presence: { message: "was blank or contained invalid characters." }
 
-  before_save :process_time
+  before_validation :set_time, if: :times_are_present?
 
-  def string_to_hash(string)
-      eval(string)
+  def times_are_present?
+    get_service_start.present? && get_service_end.present?
+  end
+
+  def times_are_valid?
+    [get_service_start, get_service_end].all? do |time|
+      time[4] != 0 && time[4].present? && time[5].present?
+    end
   end
 
   def parse_time
-    [start_time, end_time].collect do |raw_time|
-      time = string_to_hash raw_time
-      parsed_time = "#{time[4]}:#{time[5]}"
-      parsed_time << "0" if time[5] == 0
-      parsed_time
+    [get_service_start, get_service_end].map do |raw_time|
+      parsed_time = "#{raw_time[4]}:#{raw_time[5]}"
+      parsed_time << "0" if raw_time[5] == 0
     end
-
   end
 
-  def set_time_attributes
+  def set_time
     self.start_time, self.end_time = parse_time
-  end
-
-  def process_time
-    set_time_attributes
   end
 
 end
