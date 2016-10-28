622549b2d0023ac2aeff2be1256270108f6f1b47
Dom McKellar <dom.mckellar@gmail.com>
2016-10-27 21:41:46 -0700

    Reset devise views and application controllers strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   private
 
   def after_sign_in_path_for(resource_or_scope)
-    request.env['omniauth.origin'] || profile_path
+    # request.env['omniauth.origin'] || root_url#profile_path
+    new_account_path
   end
 
-  # NOTE: Adding new paramters
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :company, :account_type, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :company, :account_type, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
   end
 
 end
