a697cd4108655ab429e7a7ba690d08328daeb904
Dom McKellar <dom.mckellar@gmail.com>
2016-11-04 00:05:57 -0700

    Update oauth action

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.first_name, user.last_name = auth[:info][:name].split(/\W+/)
+      # user.first_name, user.last_name = auth[:info][:name].split(/\W+/)
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
