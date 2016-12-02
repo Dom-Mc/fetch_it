edfe3b58da6fb7e7c74bd729ebb4fb4f9877db8a
Dom McKellar <dom.mckellar@gmail.com>
2016-12-02 13:21:46 -0800

    Add orders and user to Account serializer

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,5 +1,5 @@
 class AccountSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :company, :slug, :account_type
-  has_many :phones
-  has_many :addresses
+  has_many :orders
+  has_one :user
 end
